878978fbea309805aabc1892885c8334c085a1a3
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-08-22 15:58:23 -0700

    attempt to update

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'xo-space',
-    'plugin:n/recommended',
-    'plugin:unicorn/recommended',
-  ],
-  plugins: [
-    'n',
-    'mocha',
-    'unicorn',
-  ],
-  rules: {
-    'capitalized-comments': 0,
-    'comma-dangle': ['error', 'always-multiline'],
-    'default-case': 0,
-    'no-multi-spaces': 0,
-    'n/shebang': 0,
-    curly: 0,
-    indent: ['error', 2, {SwitchCase: 0, MemberExpression: 0}],
-    quotes: ['error', 'single', {avoidEscape: true}],
-    semi: ['error', 'never'],
-    'unicorn/prevent-abbreviations': 'off',
-    'unicorn/no-await-expression-member': 'off',
-    'unicorn/no-null': 'off',
-    'unicorn/prefer-module': 'warn',
-    'logical-assignment-operators': 'off',
-  },
-  globals: {
-    describe: true,
-    it: true,
-  },
-}
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "node:url";
+import globals from "globals";
+import nodePlugin from "eslint-plugin-n";
+import js from "@eslint/js";
+import path from "node:path";
+
+// Mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+console.log(__filename);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+export default [
+  ...compat.extends( 'eslint:recommended', 'xo-space', 'plugin:n/recommended', 'plugin:unicorn/recommended'),
+  ...compat.plugins( 'mocha')
+  nodePlugin.configs["flat/recommended"],
+  ...typescript.configs.recommended,
+  ...compat.config({
+    rules: {
+      'capitalized-comments': 0,
+      'comma-dangle': ['error', 'always-multiline'],
+      'default-case': 0,
+      'no-multi-spaces': 0,
+      'n/shebang': 0,
+      curly: 0,
+      indent: ['error', 2, {SwitchCase: 0, MemberExpression: 0}],
+      quotes: ['error', 'single', {avoidEscape: true}],
+      semi: ['error', 'never'],
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/no-await-expression-member': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/prefer-module': 'warn',
+      'logical-assignment-operators': 'off',
+    },
+    globals: {
+      describe: true,
+      it: true,
+    }
+  }),
+]
+
